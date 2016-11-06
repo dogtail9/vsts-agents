755470940d862241242c34852a8cf237ff1757c3
Christer Eriksson <christer@dogtail.se>
2016-11-06 20:22:25 +0100

    custumScript

---
+++ ARMTemplate/ARMTemplate/CustomScripts/masterCustomScriptInit.sh
@@ -0,0 +1,13 @@
+# Custom Script for Linux
+apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get update
+apt-get install docker-engine
+service docker start
+systemctl enable docker
+docker swarm init
+docker service create --name=viz --publish=8080:8080/tcp --constraint=node.role==manager --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock manomarks/visualizer
