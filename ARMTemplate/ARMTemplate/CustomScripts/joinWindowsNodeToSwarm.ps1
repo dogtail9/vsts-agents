9bd7a51984e7d6f6de627b5a4207f3df14204255
Christer Eriksson <christer@dogtail.se>
2016-11-08 22:01:10 +0100

    typo

--- ARMTemplate/ARMTemplate/CustomScripts/joinWindowsNodeToSwarm.ps1
+++ ARMTemplate/ARMTemplate/CustomScripts/joinWindowsNodeToSwarm.ps1
@@ -6,7 +6,8 @@ Param(
 Install-Module Posh-SSH -Force
 $pass = convertto-securestring -String $password -AsPlainText -Force
 $cred=new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $pass
-New-SSHSession -ComputerName $dns"-master-0.westeurope.cloudapp.azure.com" -Credential $cred -Force
+$url=$dns + "-master-0.westeurope.cloudapp.azure.com"
+New-SSHSession -ComputerName $url -Credential $cred -Force
 $response=Invoke-SSHCommand -Index 0 -Command "sudo docker swarm join-token worker"
 $temp=$response.Output -replace "\\",""
 $temp=$temp -replace "To add a worker to this swarm, run the following command:",""
